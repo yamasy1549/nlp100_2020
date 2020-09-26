fde1c657bbe0ef59b0df708595867400b41fd89a
yamasy1549 <sanae@yamasy.info>
2020-09-26 20:52:52 +0900

    連続する名詞を名詞句として扱う

--- ch05/q49.py
+++ ch05/q49.py
@@ -4,6 +4,24 @@ def filter_meishi(morphs):
 def path_text(chunk_list):
     return [chunk.text() for chunk in chunk_list]
 
+def merge_meishi_ku(morphs):
+    merged_morphs = []
+    tmp_morph = None
+
+    for morph in morphs:
+        if morph.pos == "名詞":
+            if tmp_morph:
+                tmp_morph.surface += morph.surface
+                tmp_morph.base += morph.base
+            else:
+                tmp_morph = morph
+        else:
+            if tmp_morph:
+                merged_morphs.append(tmp_morph)
+                tmp_morph = None
+            merged_morphs.append(morph)
+    return merged_morphs
+
 if __name__ == "__main__":
     from q40 import read_cabocha_xmlfile
     from q41 import read_child_elements
@@ -14,6 +32,11 @@ if __name__ == "__main__":
     document = filter(lambda x:len(x) != 0, document)
 
     for sentence in document:
+        # 文節内で連続して出現する名詞たちを、名詞句として1つの形態素にまとめる
+        for chunk in sentence:
+            chunk.morphs = merge_meishi_ku(chunk.morphs)
+
+        # 最短係り受けパスを抽出する
         for i, i_chunk in enumerate(sentence):
             i_path = i_chunk.path_to_root(sentence)
             for j, j_chunk in enumerate(sentence[i+1:]):
@@ -24,8 +47,7 @@ if __name__ == "__main__":
                 if not i_meishi_list or not j_meishi_list:
                     continue
 
-                # XとYに置き換える
-                # TODO: 名詞"句"を置き換える
+                # 名詞句をXとYに置き換える
                 i_meishi_tmp = i_meishi_list[0].surface
                 j_meishi_tmp = j_meishi_list[0].surface
                 i_meishi_list[0].surface = "X"
